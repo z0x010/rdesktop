72646b3ccde193c5cab15533ef42a589c247f815
nobody <nobody@6ed3e302-cbb0-46be-b2a8-fc98bdbf2350>
2005-07-01 07:01:01 +0000

    This commit was manufactured by cvs2svn to create tag 'before_codeproject_codechanges'.

---
+++ tags/before_codeproject_codechanges/seamlessrdp/ServerExe/HookDll/hook.h
@@ -0,0 +1,51 @@
+//
+// Copyright (C) 2004-2005 Martin Wickett
+//
+
+#ifndef __CBT_H__
+#define __CBT_H__
+
+#include <windows.h>
+#include "hookdll.h"
+
+//
+// a wrapper class to access the WTSWinClipperdll
+//
+class WTSWinClipper
+{
+    protected:
+        static HINSTANCE mCbtDllHinst;
+        static SETHOOKS mSetCbtHook;
+        static REMOVEHOOKS mRemoveCbtHook;
+        static GETINSTANCECOUNT mGetInstanceCount;
+
+    public:
+        static bool Init();
+        static void Done()
+        {
+            if (mCbtDllHinst)
+                FreeLibrary(mCbtDllHinst);
+        }
+
+        static void SetCbtHook()
+        {
+            if (mCbtDllHinst)
+                mSetCbtHook();
+        }
+
+        static void RemoveCbtHook()
+        {
+            if (mCbtDllHinst)
+                mRemoveCbtHook();
+        }
+
+        static int GetInstanceCount()
+        {
+            if (mCbtDllHinst)
+                return mGetInstanceCount();
+            else
+                return 0;
+        }
+};
+
+#endif
---
+++ tags/before_codeproject_codechanges/seamlessrdp/ServerExe/HookDll/hookdll.h
@@ -0,0 +1,25 @@
+//
+// Copyright (C) 2004-2005 Martin Wickett
+//
+
+#include <windows.h>
+
+#ifndef __CBTDLL_H__
+#define __CBTDLL_H__
+
+#define CHANNELNAME "CLIPPER"
+
+#pragma once
+
+//Hooking
+typedef void (*SETHOOKS) ();
+typedef void (*REMOVEHOOKS) ();
+typedef int (*GETINSTANCECOUNT) ();
+
+//Terminal Server
+int OpenVirtualChannel();
+int CloseVirtualChannel();
+int ChannelIsOpen();
+int WriteToChannel(PCHAR buffer);
+
+#endif
